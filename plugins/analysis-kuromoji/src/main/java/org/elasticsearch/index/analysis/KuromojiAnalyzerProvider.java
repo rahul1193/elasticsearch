924a45db2d89b96b6ed245a372edcbf1e121e299
Sumanth <sumanth.bandi@sprinklr.com>
2017-03-12 15:57:36 +0530

    LST-7192: Analysis-Kuromoji plugin code chnages for ES-5.2.2 : SprJapaneseAnalyzer using "Lucene Kuromoji analyzer with Neologd dictionary Jar" (Lucene's basic dictionary is replaced with Neologd dictionary and recompiled)     discardPunctuation is set to false in SprJapaneseAnalyzer#createComponents as well as KuromojiTokenizerFactory#init

--- plugins/analysis-kuromoji/src/main/java/org/elasticsearch/index/analysis/KuromojiAnalyzerProvider.java
+++ plugins/analysis-kuromoji/src/main/java/org/elasticsearch/index/analysis/KuromojiAnalyzerProvider.java
@@ -20,31 +20,30 @@
 package org.elasticsearch.index.analysis;
 
 import org.apache.lucene.analysis.CharArraySet;
-import org.apache.lucene.analysis.ja.JapaneseAnalyzer;
 import org.apache.lucene.analysis.ja.JapaneseTokenizer;
+import org.apache.lucene.analysis.ja.SprJapaneseAnalyzer;
 import org.apache.lucene.analysis.ja.dict.UserDictionary;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
 
-import java.util.Set;
+import java.util.Collections;
 
 /**
  */
-public class KuromojiAnalyzerProvider extends AbstractIndexAnalyzerProvider<JapaneseAnalyzer> {
+public class KuromojiAnalyzerProvider extends AbstractIndexAnalyzerProvider<SprJapaneseAnalyzer> {
 
-    private final JapaneseAnalyzer analyzer;
+    private final SprJapaneseAnalyzer analyzer;
 
     public KuromojiAnalyzerProvider(IndexSettings indexSettings, Environment env, String name, Settings settings) {
         super(indexSettings, name, settings);
-        final Set<?> stopWords = Analysis.parseStopWords(env, settings, JapaneseAnalyzer.getDefaultStopSet());
         final JapaneseTokenizer.Mode mode = KuromojiTokenizerFactory.getMode(settings);
         final UserDictionary userDictionary = KuromojiTokenizerFactory.getUserDictionary(env, settings);
-        analyzer = new JapaneseAnalyzer(userDictionary, mode, CharArraySet.copy(stopWords), JapaneseAnalyzer.getDefaultStopTags());
+        analyzer = new SprJapaneseAnalyzer(userDictionary, mode, CharArraySet.EMPTY_SET, Collections.emptySet(), env.settings());
     }
 
     @Override
-    public JapaneseAnalyzer get() {
+    public SprJapaneseAnalyzer get() {
         return this.analyzer;
     }
 
